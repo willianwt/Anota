ae5387814c524ab21eb52002b2d4b49823d9d253
Willian <willianwt@gmail.com>
2019-09-03 15:01:31 -0300

    iniciado listagem de curso, ja é possivel recuperar a lista dos cursos cadastrados.

--- app/src/main/java/com/williantaiguara/anota/activity/MeusCursosActivity.java
+++ app/src/main/java/com/williantaiguara/anota/activity/MeusCursosActivity.java
@@ -5,14 +5,41 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.williantaiguara.anota.R;
+import com.williantaiguara.anota.adapter.AdapterAdicionarDisciplinas;
+import com.williantaiguara.anota.adapter.AdapterCursos;
+import com.williantaiguara.anota.config.ConfiguracaoFirebase;
+import com.williantaiguara.anota.helper.Base64Custom;
+import com.williantaiguara.anota.model.Curso;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MeusCursosActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewListaCursos;
+    private AdapterCursos adapterCursos;
+    private DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
+    private DatabaseReference listaCursosRef;
+    private Curso curso;
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    private List<Curso> cursos = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +48,18 @@ public class MeusCursosActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Meus Cursos");
         setSupportActionBar(toolbar);
+
+        recyclerViewListaCursos = findViewById(R.id.recyclerViewListaCursos);
+
+        adapterCursos = new AdapterCursos(cursos, this);
+
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerViewListaCursos.setLayoutManager(layoutManager);
+        recyclerViewListaCursos.setHasFixedSize(true);
+        recyclerViewListaCursos.setAdapter(adapterCursos);
+
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -33,4 +72,36 @@ public class MeusCursosActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    public void recuperarCursos(){
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.CodificarBase64(emailUsuario);
+
+        Query query = firebaseRef.child("usuarios").child(idUsuario).child("cursos");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                cursos.clear();
+
+                for (DataSnapshot dados: dataSnapshot.getChildren()){
+                    Curso curso = dados.getValue(Curso.class);
+                    curso.setKey(dados.getKey());
+                    //TODO: na proxima activity, pegar os semestres do curso escolhido.
+                    cursos.add(curso);
+                }
+                adapterCursos.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recuperarCursos();
+    }
 }
---
+++ app/src/main/java/com/williantaiguara/anota/adapter/AdapterCursos.java
@@ -0,0 +1,59 @@
+package com.williantaiguara.anota.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.williantaiguara.anota.R;
+import com.williantaiguara.anota.model.Curso;
+
+import java.util.List;
+
+public class AdapterCursos extends RecyclerView.Adapter<AdapterCursos.MyViewHolder> {
+
+    private List<Curso> cursoList;
+    private Context context;
+
+    public AdapterCursos(List<Curso> cursos, Context c){
+        this.cursoList = cursos;
+        this.context = c;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemCurso = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.adapter_cursos, parent, false);
+
+        return new MyViewHolder(itemCurso);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Curso curso = cursoList.get(position);
+
+        holder.nomeCurso.setText(curso.getKey());
+    }
+
+    @Override
+    public int getItemCount() {
+        return cursoList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView nomeCurso;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            nomeCurso = itemView.findViewById(R.id.txNomeDoCurso);
+        }
+    }
+}
